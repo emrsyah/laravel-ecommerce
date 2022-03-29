fae5803325bb93cf5c8aec95bdf828ceb5219ae4
Emirsyah <muhammademir48@gmail.com>
2022-03-29 20:11:01 +0700

    first init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Home</title>
+</head>
+<body class="bg-[#fefefe]">
+    @include('layout.navbar')
+
+    <!-- Hero -->
+    <div class="mx-20 my-10 rounded-full">
+        <!-- Kiri -->
+        <div>
+            <h1 class="text-8xl font-semibold max-w-3xl leading-[1.15]">Explore Your Own Style</h1>
+            <p class="mt-6 text-gray-600 max-w-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam dolor neque saepe, voluptate quas esse natus.</p>
+            <a href="" class="button-animasi">Explore Now</a>
+        </div>
+        
+        <!-- Kanan -->
+        <div>
+
+        </div>
+    </div>
+
+
+
+</body>
+</html>
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="border-gray-400 border-b-[1px] bg-white" >
+    <div class="flex justify-between mx-20 my-4 justify-center items-center">
+        <h5 class="text-3xl font-semibold  tracking-wide">Blanja</h5>
+        <div class="flex gap-8 justify-center items-center">
+            <a href="" class="font-semibold text-gray-500 hover:text-black">Login</a>
+            <a href="" class="bg-gray-800 text-white py-3 px-6 rounded-full font-semibold hover:bg-black">Sign up free</a>
+        </div>
+    </div>
+</nav>

b1f0ff7ee375b47c7a877e09638b39eb942c263f
Emirsyah <muhammademir48@gmail.com>
2022-03-29 21:21:53 +0700

    hero added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,17 +11,20 @@
     @include('layout.navbar')
 
     <!-- Hero -->
-    <div class="mx-20 my-10 rounded-full">
+    <div class="mx-20 my-12 rounded-full flex justify-between items-center gap-6">
         <!-- Kiri -->
-        <div>
-            <h1 class="text-8xl font-semibold max-w-3xl leading-[1.15]">Explore Your Own Style</h1>
-            <p class="mt-6 text-gray-600 max-w-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam dolor neque saepe, voluptate quas esse natus.</p>
+        <div class="flex-1">
+            <h1 class="text-8xl font-semibold leading-[1.05]">Everything your home deserves</h1>
+            <p class="mt-4 mb-10 text-gray-600 max-w-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem, magni praesentium possimus voluptatibus libero explicabo. Accusamus.</p>
             <a href="" class="button-animasi">Explore Now</a>
         </div>
         
         <!-- Kanan -->
-        <div>
-
+        <div class="border-black flex-1 relative flex flex-col">
+            <!-- <img src="{{ URL::to('/assets/gambar1.jpg') }}" alt="" class="h-3/5 rounded-3xl"> -->
+            <img src="{{ URL::to('/assets/gambar4.jpg') }}" alt="" class="w-8/12 h-auto m-auto block max-w-max z-10 relative right-0 top-0">
+            <img src="{{ URL::to('/assets/gambar1.jpg') }}" alt="" class="w-8/12 h-auto m-auto block max-w-max absolute top-8 left-8 z-0">
+            <!-- <img src="{{ URL::to('/assets/gambar2.jpg') }}" alt="" class="w-2/4 rounded-3xl block "> -->
         </div>
     </div>
 
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -1,7 +1,8 @@
-<nav class="border-gray-400 border-b-[1px] bg-white" >
+<nav class="bg-white" >
     <div class="flex justify-between mx-20 my-4 justify-center items-center">
-        <h5 class="text-3xl font-semibold  tracking-wide">Blanja</h5>
+        <h5 class="text-3xl font-semibold  tracking-wide">Funitura</h5>
         <div class="flex gap-8 justify-center items-center">
+            <a href="" class="font-semibold text-gray-500 hover:text-black">Home</a>
             <a href="" class="font-semibold text-gray-500 hover:text-black">Login</a>
             <a href="" class="bg-gray-800 text-white py-3 px-6 rounded-full font-semibold hover:bg-black">Sign up free</a>
         </div>
